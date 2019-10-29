f9d1c85f8030a3a0ff1b39e87d32d1b7be4e1591
Jaheed Haynes <jaheedhaynes@pursuit.org>
2019-10-29 11:22:02 -0400

    Initial Commit

---
+++ Flashlight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashlight
+//
+//  Created by Jaheed Haynes on 10/29/19.
+//  Copyright © 2019 Jaheed Haynes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
